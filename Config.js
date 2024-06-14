d4b26c9fbd34828e0c164652b3b5e35ce6b55cfe
Kevinvin-prog <kevinafune576@gmail.com>
2024-06-14 15:43:20 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 //contact details
 global.ownernumber = [ process.env.OWNER_NUMBER || "254792010505"],
-global.ownername = process.env.OWNER_NAME || "kevinvin tech",
+global.ownername = process.env.OWNER_NAME || "professor",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
